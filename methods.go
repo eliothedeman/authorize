7ebda9b3ac3cda15bbe0cc7223350c2a4680377b
Eliot Hedeman <ehedeman@barracuda.com>
2015-04-18 20:14:03 -0400

    return back single values instead of response objects

--- methods.go
+++ methods.go
@@ -3,18 +3,20 @@ package authorize
 import "github.com/eliothedeman/authorize/cim"
 
 // create a new customer profile in authorize.net
-func (c *Client) CreateCustomerProfile(p cim.Profile) (resp *cim.CreateCustomerProfileResponse, err error) {
+func (c *Client) CreateCustomerProfile(p cim.Profile) (string, err error) {
 	req := &cim.CreateCustomerProfileRequest{}
 	req.Profile = p
 	r := c.Do(req)
 	err = r.Err
-	return r.ResponseStruct.(*cim.CreateCustomerProfileResponse), err
+	resp := r.ResponseStruct.(*cim.CreateCustomerProfileResponse)
+	return resp.CustomerProfileId, err
 }
 
-func (c *Client) GetCustomerProfile(profileId string) (resp *cim.GetCustomerProfileResponse, err error) {
+func (c *Client) GetCustomerProfile(profileId string) (*cim.Profile, err error) {
 	req := &cim.GetCustomerProfileRequest{}
 	req.CustomerProfileId = profileId
 	r := c.Do(req)
 	err = r.Err
-	return r.ResponseStruct.(*cim.GetCustomerProfileResponse), err
+	resp := r.ResponseStruct.(*cim.GetCustomerProfileResponse)
+	return resp.Profile, err
 }
