7ebef82fa803de715b54ab2ca0f91593b13537bb
Max Titov <eolexe@gmail.com>
2015-05-25 20:23:50 +0700

    log.SetFlags on init mess application log configuraions

--- testing.go
+++ testing.go
@@ -12,16 +12,17 @@ const (
 )
 
 func init() {
-	log.SetFlags(log.Llongfile)
 	rand.Seed(time.Now().Unix())
 }
 func NewTestClient() *Client {
+	log.SetFlags(log.Llongfile)
 	c := NewClient(TEST_API_LOGIN_ID, TEST_API_TRANSACTION_KEY)
 	c.url = SANDBOX_URL
 	return c
 }
 
 func NewTestClientWithCredentials(name, transactionKey string) *Client {
+	log.SetFlags(log.Llongfile)
 	c := NewClient(name, transactionKey)
 	c.url = SANDBOX_URL
 	return c
